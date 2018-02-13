436af3c543f8c8b66d50583e02c9c85d870463bc
JFRabbit <qwertjkl9090@163.com>
2018-02-13 10:21:44 +0800

    annotate some code, cause it will raise exception debug

--- common/__init__.py
+++ common/__init__.py
@@ -1,8 +1,8 @@
-import sys
-
-__PROJECT_NAME = "scorpio"
-
-PROJECT_PATH = sys.path[0][0:sys.path[0].index(__PROJECT_NAME)] + __PROJECT_NAME
-
-if __name__ == "__main__":
-    print(PROJECT_PATH)
+# import sys
+#
+# __PROJECT_NAME = "scorpio"
+#
+# PROJECT_PATH = sys.path[0][0:sys.path[0].index(__PROJECT_NAME)] + __PROJECT_NAME
+#
+# if __name__ == "__main__":
+#     print(PROJECT_PATH)
