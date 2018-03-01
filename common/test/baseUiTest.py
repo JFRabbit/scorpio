39be72d8f5450bc15070b0a8bedb896276526df0
JFRabbit <qwertjkl9090@163.com>
2018-03-01 16:36:48 +0800

    add exception_handler

--- common/test/baseUiTest.py
+++ common/test/baseUiTest.py
@@ -3,6 +3,27 @@ from common.test import *
 from common.ui.uiDriverManager import *
 
 
+def exception_handler(is_open=False):
+    def foo(func):
+        def bar(*args, **kwargs):
+            if is_open:
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    for i in args:
+                        if isinstance(i, UITestBase):
+                            i.log.exception(e)
+                    time.sleep(1)
+                    input("input any key to continue:")
+                    raise e
+            else:
+                return func(*args, **kwargs)
+
+        return bar
+
+    return foo
+
+
 class UITestBase(unittest.TestCase):
     def setUp(self):
         self.manager = UIDriverManager()
